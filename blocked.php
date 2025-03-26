41ef7a13d4511b75bdee7027db641f60b1103e84
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2025-03-26 19:22:48 +0700

    Create blocked.php

---
+++ blocked.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cosmic Esport</title>
+  <link rel="icon" type="image/*" href="../src/logo.png">
+</head>
+<body>
+  <h1>Access Blocked</h1>
+  <p>Sorry, bots are not allowed to access this page.</p>
+</body>
+</html>
